b05231a22297115d674378519f98b890336b63a2
Bence Meszaros <bence.me@gmail.com>
2016-01-30 15:00:51 -0500

    Initial implementation of `memory.js`

--- memory.js
+++ memory.js
@@ -5,49 +5,92 @@ function Memory () {
 	this.store = [];
 }
 
+//
+// Inserts one or more structs into the store, giving each one a unique id,
+// and returns the id inserted structs.  A list of structs is returned if a 
+// list structs is provided, otherwise a single struct is returned.
+// Input structs do not have id properties.
+//
 Memory.prototype.create = function (structs, callback) /* callback(error, structs) */ {
+	if (structs.constructor == Array) {
+		var processed_structs = [];
 
-    //
-	// Inserts one or more structs into the store, giving each one a unique id,
-	// and returns the id inserted structs.  A list of structs is returned if a 
-	// list structs is provided, otherwise a single struct is returned.
-	// Input structs do not have id properties.
-	//
+		var wrap_single_callback = function(_err, _stru) {
+			if (_err) {
+				erred = true;
+				return callback(_err, null);
+			}
 
+			processed_structs.push(_stru);
+		};
+
+		for (var struct in structs) {
+			var erred = false;
+
+			Memory.create(struct, wrap_single_callback);
+
+			if (erred) return null;
+		}
+
+		callback(null, processed_structs);
+	}
+	else {
+		if (typeof structs.id !== "undefined")
+			return callback(new Error("Struct ID must NOT be set"), null);
+
+		this.id += 1;
+		structs.id = this.id;
+		this.store[structs.id] = structs;
+
+		callback(null, structs);
+	}
 };
 
+//
+// Returns the struct with the provided id from the store.
+//
 Memory.prototype.read = function (id, callback) /* callback(error, struct) */ {
+	if (typeof this.store[id] === "undefined")
+		return callback(new Error("ID not set"), null);
 
-	//
-	// Returns the struct with the provided id from the store.
-	//
+	var struct = this.store[id];
 
+	callback(null, struct);
 };
 
+//
+// Updates the struct in the store with the provided struct, which must contain
+// the id property.  Returns the updated attachment struct.
+//
 Memory.prototype.update = function (struct, callback) /* callback(error, struct) */ {
+	if (typeof struct.id === "undefined")
+		return callback(new Error("Struct ID must be set"), null);
+	if (typeof this.store[struct.id] === "undefined")
+		return callback(new Error("ID not set"), null);
 
-    //
-	// Updates the struct in the store with the provided struct, which must contain
-	// the id property.  Returns the updated attachment struct.
-	//
+	this.store[struct.id] = struct;
 
+	callback(null, struct);
 };
 
+//
+// Removes the struct from the store with the provided id.
+//
 Memory.prototype.delete = function (id, callback) /* callback(error) */ {
+	if (typeof this.store[id] === "undefined")
+		return callback(new Error("ID not set"), null);
 
-    //
-	// Removes the struct from the store with the provided id.
-	//
-
+	delete this.store[id];
 };
 
+//
+// Returns a list of structs with the provided property that have the
+// provided value - i.e. struct[property] === value
+//
 Memory.prototype.find = function (property, value, callback) /* callback(error, structs) */ {
-
-    //
-	// Returns a list of structs with the provided property that have the
-	// provided value - i.e. struct[property] === value
-	//
-
+	callback(null, this.store.filter(function(struct) {
+		return !!struct && struct[property] === value;
+	}));
 };
 
 ///////////////////// PUBLIC SYMBOLS /////////////////////
