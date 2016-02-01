88b50d5f2047b04edd9070eadc1fc8cf694b785b
Bence Meszaros <bence.me@gmail.com>
2016-02-01 10:19:28 -0500

    Re-indent `client.js`

--- client.js
+++ client.js
@@ -1,59 +1,59 @@
 (function() {
-    'use strict';
+	'use strict';
 
-    angular
-    	.module('mw', []);
+	angular
+		.module('mw', [ngResource]);
 
-    //////////// ObjectList ////////////
+	//////////// ObjectList ////////////
 
-    angular
-        .module('mw')
-        .controller('ObjectList', ObjectList);
+	angular
+		.module('mw')
+		.controller('ObjectList', ObjectList);
 
-    ObjectList.$inject = ['$scope'];
+	ObjectList.$inject = ['$scope'];
 
-    function ObjectList($scope) {
-    	$scope.objects = [
-    		{ id: 1, type: 'Student'   , attachable:true , name  :'Jim'     , degree: 'BSc'    },
-    		{ id: 1, type: 'Course'    , attachable:true , name  :'MEAN'    , desc  : 'Using the MEAN stack for web applications.' },
-    		{ id: 3, type: 'Student'   , attachable:true , name  :'Frank'   , degree: 'BA'     },
-    		{ id: 2, type: 'Course'    , attachable:true , name  :'Physics' , desc  : 'Really hard math.' },
-    		{ id: 1, type: 'Classroom' , attachable:false, course:'MEAN'    , day   : 'Friday' },
-    		{ id: 2, type: 'Student'   , attachable:true , name  :'Sue'     , degree: 'BSc'    },
-    		{ id: 2, type: 'Classroom' , attachable:false, course:'Physics' , day   : 'Monday' }
-    	];
-    }
+	function ObjectList($scope) {
+		$scope.objects = [
+			{ id: 1, type: 'Student'   , attachable:true , name  :'Jim'     , degree: 'BSc'    },
+			{ id: 1, type: 'Course'    , attachable:true , name  :'MEAN'    , desc  : 'Using the MEAN stack for web applications.' },
+			{ id: 3, type: 'Student'   , attachable:true , name  :'Frank'   , degree: 'BA'     },
+			{ id: 2, type: 'Course'    , attachable:true , name  :'Physics' , desc  : 'Really hard math.' },
+			{ id: 1, type: 'Classroom' , attachable:false, course:'MEAN'    , day   : 'Friday' },
+			{ id: 2, type: 'Student'   , attachable:true , name  :'Sue'     , degree: 'BSc'    },
+			{ id: 2, type: 'Classroom' , attachable:false, course:'Physics' , day   : 'Monday' }
+		];
+	}
 
-    //////////// mwObject ////////////
+	//////////// mwObject ////////////
 
-    // Provide a way to add attachments to the
-    // object by dragging & dropping files into
-    // the .object div.
-    angular
-        .module('mw')
-        .directive('mwObject', mwObject);
+	// Provide a way to add attachments to the
+	// object by dragging & dropping files into
+	// the .object div.
+	angular
+		.module('mw')
+		.directive('mwObject', mwObject);
 
-    function mwObject () {
-        return {
-        	templateUrl: 'object.html',
-            controller: ObjectController,
-            restrict: 'E',
-            transclude:true,
-            scope: {
-            	object : '='
-            }
-        };
-    }
+	function mwObject () {
+		return {
+			templateUrl: 'object.html',
+			controller: ObjectController,
+			restrict: 'E',
+			transclude:true,
+			scope: {
+				object : '='
+			}
+		};
+	}
 
 	ObjectController.$inject = ['$scope'];
 
-    function ObjectController ($scope) {
-    	$scope.editing = false;
-    	$scope.edit    = function () { $scope.editing = true;  };
-    	$scope.save    = function () { $scope.editing = false; };
-   	}
+	function ObjectController ($scope) {
+		$scope.editing = false;
+		$scope.edit    = function () { $scope.editing = true;  };
+		$scope.save    = function () { $scope.editing = false; };
+	}
 
-    //////////// mwLocking ////////////
+	//////////// mwLocking ////////////
 
 	//
 	//  Provide a directive that:
@@ -61,30 +61,30 @@
 	//    when object.editing is true
 	//  o Removes the editing class & disabled the
 	//    element when object.editing is false
-    //
-    angular
-        .module('mw')
-        .directive('mwLocking', mwLocking);
+	//
+	angular
+		.module('mw')
+		.directive('mwLocking', mwLocking);
 
-    function mwLocking () {
-        return {
-            restrict: 'A',
-            link: function(scope, element, attr) {
-                scope.$watch('$parent.editing', function(isEditing) {
-                    if (isEditing) {
-                        element.addClass('editing');
-                        element.attr('disabled', false);
-                    }
-                    else {
-                        element.removeClass('editing');
-                        element.attr('disabled', true);
-                    }
-                });
-            }
-        };
-    }
+	function mwLocking () {
+		return {
+			restrict: 'A',
+			link: function(scope, element, attr) {
+				scope.$watch('$parent.editing', function(isEditing) {
+					if (isEditing) {
+						element.addClass('editing');
+						element.attr('disabled', false);
+					}
+					else {
+						element.removeClass('editing');
+						element.attr('disabled', true);
+					}
+				});
+			}
+		};
+	}
 
-    //////////// mwAttachments ////////////
+	//////////// mwAttachments ////////////
 
 	//
 	//  Provide a directive that:
@@ -98,18 +98,21 @@
 	//  o a way to open up a file list and add one or
 	//    more files but does not display a 
 	//    <input type='file'/> element
-    //
-    angular
-        .module('mw')
-        .directive('mwAttachments', mwAttachments);
+	//
+	angular
+		.module('mw')
+		.directive('mwAttachments', mwAttachments);
 
-    function mwAttachments () {
-        return {
-            restrict: 'E',
-            scope: {
-            	object : '='
-            }
-        };
-    }
+	function mwAttachments () {
+		return {
+			restrict: 'E',
+			scope: {
+				object : '='
+			},
+			link: function(scope) {
+				
+			}
+		};
+	}
 
 })();
